0c93a5f8e87e44f18ddd53a83fac008cc79412d6
Tuong Le <tuong@elixirtech.com>
2023-12-09 09:59:05 +0700

    remove duplicated dependency 'testng'

--- src/main/java/api/BaseApi.java
+++ src/main/java/api/BaseApi.java
@@ -4,5 +4,5 @@ import io.restassured.RestAssured;
 
 public class BaseApi {
     final static String BASE_URL = "";
-    private RestAssured restAssured = new RestAssured();
+    private final RestAssured restAssured = new RestAssured();
 }
