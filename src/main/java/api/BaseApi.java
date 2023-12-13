18d94558e69a79dbee5e1fd2734f00e9b26b457a
TuongLe <tuong@elixirtech.com>
2023-12-13 10:44:19 +0700

    - remove unnecessary classes

--- src/main/java/api/BaseApi.java
+++
@@ -1,8 +0,0 @@
-package api;
-
-import io.restassured.RestAssured;
-
-public class BaseApi {
-    final static String BASE_URL = "";
-    private final RestAssured restAssured = new RestAssured();
-}
